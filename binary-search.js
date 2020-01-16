c375ab1c343ce54050bc49dd24282815ab1ab0ea
Alan Koger <adkoge@gmail.com>
2020-01-16 18:44:16 -0500

    Adjusted variable names and comments

--- binary-search.js
+++ binary-search.js
@@ -1,36 +1,42 @@
-let listOfNumbers = [1, 3, 55, 27, 12, 12];
-
-const binarySearch = (list, target) => {
-  // Track low & high to know where we are
-  let high = list.length - 1;
-  let low = 0;
-  let mid = 0;
-  
-  // While we haven't found the target
-  while(low <= high) {
-    mid = Math.floor((low + high) / 2);
-    console.log("Mid is: " + mid);
-
-    let guess = list[mid];
-    console.log("Set guess to: " + guess);
-    if (guess == target) {
-      return console.log(`Target of ${mid} found`);
-    } else if (target > guess) {
-      console.log(`Search left half`);
-      low = mid + 1;
-    } 
-    else {
-      console.log(`Search right half`);
-      high = mid - 1;
-    }
-  }
-  return -1;
-}
+let arrayToSearch = [1, 3, 55, 27, 12, 12];
 
 // Sort the array
-let sorted = listOfNumbers.sort(function(a,b) {
-  return a-b;
+/* Look at MDN array.protoype.sort to see the sort
+implementation. This allows you to compare numbers 
+and sort the array in ascending order */
+let sortedArray = arrayToSearch.sort(function (a, b) {
+  return a - b;
 });
 
-// Test the binary search algorithm
-binarySearch(sorted, 12);
+console.log(`Sorted array is: ${sortedArray}`);
+
+const binarySearch = (arrayOfNumbers, target) => {
+  // Track low index & high index to know where we are
+  let highIndex = arrayOfNumbers.length - 1;
+  let lowIndex = 0;
+  let middleIndex = 0;
+  
+  // While we haven't found the target
+  while(lowIndex <= highIndex) {
+    middleIndex = Math.floor((lowIndex + highIndex) / 2);
+    console.log("middleIndex is now: " + middleIndex);
+
+    let guess = arrayOfNumbers[middleIndex];
+    console.log("Setting guess to: " + guess);
+    if (guess == target) {
+      return console.log(`Target found at index ${middleIndex} with the value ${target}`);
+    } else if (target > guess) {
+      console.log(`Searching left half`);
+      lowIndex = middleIndex + 1;
+    } 
+    else {
+      console.log(`Searching right half`);
+      highIndex = middleIndex - 1;
+    }
+  }
+  return console.log(`${target} was not found in the array`);
+}
+
+// Take assorted array and apply our binarySearch algorithm
+// binarySearch(sortedArray, 12);
+binarySearch(sortedArray, 200);
