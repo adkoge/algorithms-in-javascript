06986c70c50e727cb798fd1d7009395b672c7f8d
Alan Koger <adkoge@gmail.com>
2020-01-16 08:14:46 -0500

    Fimnished initial binary search algorithm

--- binary-search.js
+++ binary-search.js
@@ -1,28 +1,36 @@
+let listOfNumbers = [1, 3, 55, 27, 12, 12];
+
 const binarySearch = (list, target) => {
   // Track low & high to know where we are
-  let low = 0;
   let high = list.length - 1;
-
+  let low = 0;
+  let mid = 0;
+  
   // While we haven't found the target
   while(low <= high) {
-    console.log("Get mid value");
-    let mid = (low + high) / 2;
+    mid = Math.floor((low + high) / 2);
     console.log("Mid is: " + mid);
+
     let guess = list[mid];
     console.log("Set guess to: " + guess);
-    if (guess === target) {
-      return console.log("Target found at index: " + mid);
-    };
-    if (guess > target) {
-      console.log("Search left half");
-      return high = mid - 1;
-    } else {
-      console.log("Search right half");
-      return low = mid + 1;
+    if (guess == target) {
+      return console.log(`Target of ${mid} found`);
+    } else if (target > guess) {
+      console.log(`Search left half`);
+      low = mid + 1;
+    } 
+    else {
+      console.log(`Search right half`);
+      high = mid - 1;
     }
-    return  "null";
   }
+  return -1;
 }
 
-numberList = [1, 3, 5, 7, 9];
-console.log(binarySearch(numberList, 1));
+// Sort the array
+let sorted = listOfNumbers.sort(function(a,b) {
+  return a-b;
+});
+
+// Test the binary search algorithm
+binarySearch(sorted, 12);
