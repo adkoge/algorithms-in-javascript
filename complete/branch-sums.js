da454efe4e6c8c9ae73570e636dce453d8e23f4b
Alan Koger <adkoge@gmail.com>
2020-01-28 20:04:28 -0500

    Added branch sums. Need to refactor

---
+++ complete/branch-sums.js
@@ -0,0 +1,27 @@
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// O(n) time | O(n) space - where n is the number of nodes in the Binary Tree
+function branchSums(root) {
+  const sums = [];
+  calculateBranchSums(root, 0, sums);
+  return sums;
+}
+
+function calculateBranchSums(node, runningSum, sums) {
+  if (!node) return;
+
+  const newRunningSum = runningSum + node.value;
+  if (!node.left && !node.right) {
+    sums.push(newRunningSum);
+    return;
+  }
+
+  calculateBranchSums(node.left, newRunningSum, sums);
+  calculateBranchSums(node.right, newRunningSum, sums);
+}
